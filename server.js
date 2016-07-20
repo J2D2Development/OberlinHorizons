f4f90f6653d325051352e1d74fd29a3b1cc9de76
J2D2Development <joedriscoll79@gmail.com>
2016-07-20 06:01:05 -0700

    Express serving all files- trying to work with react

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
     // console.log(req);
